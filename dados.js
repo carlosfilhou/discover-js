d482d51bc6e618f3be3644dd6f0345ea606f3d2e
Carlos Augusto <carlosfilho.canal@icloud.com>
2022-02-19 19:55:53 -0300

    doc-dados

---
+++ dados.js
@@ -0,0 +1,20 @@
+// Exemplo de obejto
+console.log('Isso é um objeto e suas propriedades')
+console.log({
+  name: "Carlos",
+  idade: 26,
+  andar: function(){
+    console.log('andar')
+  }
+})
+
+// Exemplo de array ou vetores
+console.log('Isso é um array ou vetor')
+console.log([
+  "Carlos",
+  26,
+  "Leite",
+  "Ovos"
+])
+
+// 
