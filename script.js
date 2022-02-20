54b29c6fc6b529d411a3c22cc932b27c2d1b4962
Carlos Augusto <carlosfilho.canal@icloud.com>
2022-02-19 23:26:44 -0300

    docs-scope

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 console.log('bem vindos ao starter')
 console.log('eu sou carlos augusto')
 
+// declarando
+var name
+
+// assignment or atribuir valor
+name = 'Carlos'
+
+// que tipo de dado foi colocado na variável
+console.log(typeof name)
+
