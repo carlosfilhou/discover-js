47418534d6078c15c4a72214754365f7678d0e29
Carlos Augusto <carlosfilho.canal@icloud.com>
2022-02-20 16:13:24 -0300

    feat-function

---
+++ functions.js
@@ -0,0 +1,14 @@
+// criar um aplicativo de frases motivacionais
+
+// declaração da função
+function createPhrases() {
+  console.log('Estudar é muito bom')
+  console.log('Paciência e persistência')
+  console.log('Revisão é mãe do aprendizado')
+}
+
+// chamar a função
+createPhrases()
+createPhrases()
+
+console.log('Fim do Código')
