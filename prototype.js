489cf39b2a817441ca8e9aadf9efb6ed48489c30
Carlos Augusto <carlosfilho.canal@icloud.com>
2022-02-24 01:56:49 -0300

    feat-conversionCoersion

--- prototype.js
+++ prototype.js
@@ -1,2 +1,8 @@
 //prototype-based - linguagem baseada em protótipo
 //prototype chain - cadeia de protótipo
+
+//type coersion (o próprio java script forçou o número 5 a virar uma string para concatenar, ou seja, de maneira forçosa)
+console.log('9' + 5)
+
+//type conversion (eu MANUALMENTE transformo um tipo de dado em outro)
+console.log(Number('9') + 5)
