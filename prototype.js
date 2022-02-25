e2c7d32b7f865482a60ce4fd6cc5bec06b1d7e4e
Carlos Augusto <carlosfilho.canal@icloud.com>
2022-02-24 22:24:26 -0300

    docs-prototype

--- prototype.js
+++ prototype.js
@@ -6,3 +6,13 @@ console.log('9' + 5)
 
 //type conversion (eu MANUALMENTE transformo um tipo de dado em outro)
 console.log(Number('9') + 5)
+
+//manipulando STRINGS e NÚMEROS
+//transformar string em número e número em string
+let string = "123"
+console.log(Number(string))
+
+//agora o contrário, transformando um número em uma string
+let number = 123
+console.log(String(number))
+
